f9071c2a5e3adac6a935664e67ab4e55980ee0a3
kyledunne <kyledunne@berkeley.edu>
2019-11-30 07:45:58 -0500

    Outline graphics interface

--- src/graphics.kt
+++ src/graphics.kt
@@ -1,3 +1,22 @@
+import java.awt.Color
+import java.awt.Font
+
 fun drawCard(card: BoardMinion, x: Int, y: Int) {
+
+}
+
+fun drawImage(x: Int, y: Int, w: Int, h: Int) {
+
+}
+
+fun drawText(text: Text, x: Int, y: Int) {
+
+}
+
+fun drawTriangle(x1: Int, y1: Int, x2: Int, y2: Int, x3: Int, y3: Int, color: Color) { //TODO give color a default value
+
+}
+
+class Text(var string: String, var font: Font, var color: Color) { //TODO give font and color defaults
 
 }
